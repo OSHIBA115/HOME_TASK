2570d871cc2fd0dadf11af5c05e05ce15529aa32
OSHIBA115 <ohina482@gmail.com>
2026-08-15 20:20:21 +0900

    問１TASK３書き込み完了

--- jackpot/2026_Summer_Task3.cpp
+++ jackpot/2026_Summer_Task3.cpp
@@ -1,24 +1,30 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+
 using namespace std;
 
 int main(void)
 {
-	const int CARD = 3;
-	const int CARD_NUMBER = 10;
+	//定数
+	const int CARD = 3;			//カード枚数＝３枚
+	const int CARD_NUMBER = 10;	//カードの内容＝10まで
 
-	int i, hit = 0;
-	int playerCard[CARD];
-	int enemyCard[CARD];
-	int plyChoice[CARD];
-	int emyChoice[CARD];
+	//変数宣言int型
+	int i, hit = 0;			//繰り返し用i、当たったカードの枚数カウント用hit
+	int playerCard[CARD];	//プレイヤーの手札
+	int enemyCard[CARD];	//CPUの手札
+	int plyChoice[CARD];	//プレイヤーが選んだカード用
+	int emyChoice[CARD];	//CPUが選んだカード用
 
-	bool trunFlag = true;
-	bool judgementFlag = false;
+	//変数宣言bool型
+	bool trunFlag = true;		//ターン入れ替え用
+	bool judgementFlag = false;	//勝ち負け判定用
 
+	//乱数初期化
 	srand((unsigned int)time(NULL));
 
+	//手札をランダムに配布
 	for (i = 0; i < CARD; i++)
 	{
 		playerCard[i] = rand() % CARD_NUMBER;
@@ -28,6 +34,7 @@ int main(void)
 	cout << "3マスの数字あてゲームプレイヤーとCPUが、お互いに隠し持った3つの数字を当てるターン制のゲーム" << "\n";
 	cout << "あなたの配られてカードは," << "\n";
 
+	//プレイヤーに配ったカードの内容を表示
 	for (i = 0; i < CARD; i++)
 	{
 		cout << i + 1 << "番目" << playerCard[i] << "\n";
@@ -35,16 +42,19 @@ int main(void)
 
 	cout << "===================   GAME STRAT   ============================" << "\n";
 
+	//ゲームループ
 	while (true)
 	{
-
+		//trueflagがtrueの場合はプレイヤーのターン
 		if (trunFlag)
 		{
-
+			//hit初期化
 			hit = 0;
 			cout << "PLAYER TRUN 「数字の0から9までの数字を3つ選んでください」\n";
+			//プレイヤー数字選択（３回繰り返し）
 			for (i = 0; i < CARD; i++)
 			{
+				//入力チェック含めた入力
 				while (true)
 				{
 					cin >> plyChoice[i];
@@ -60,9 +70,10 @@ int main(void)
 				}
 			}
 
-
+			//選択された数があっているか確認
 			for (i = 0; i < CARD; i++)
 			{
+				//正しい場合hitを＋１
 				if (plyChoice[i] == enemyCard[i])
 				{
 					cout << "Hit,";
@@ -74,30 +85,35 @@ int main(void)
 				}
 			}
 			cout << "\n";
+			//もし3つあっていたら勝ち負け判定をプレイヤーの勝ちに設定してループを終了
 			if (hit == 3)
 			{
 				cout << "3Hit!!\n";
 				judgementFlag = true;
 				break;
 			}
-
+			//当たった数が３つ未満の場合ターンを変更
 			cout << "\n";
 			trunFlag = false;
 		}
+		//CPUのターンの処理
 		else
 		{
+			//hit初期化
 			hit = 0;
 
 			cout << "ENEMY TRUN \n";
+			//CPUの選択をランダムで決定＆表示
 			for (i = 0; i < CARD; i++)
 			{
 				emyChoice[i] = rand() % CARD_NUMBER;
 
 				cout << i + 1 << "番目" << emyChoice[i] << "\n";
 			}
-
+			//CPUの選択の正否をチェック
 			for (i = 0; i < CARD; i++)
 			{
+				//当たっている数だけhitを＋
 				if (emyChoice[i] == playerCard[i])
 				{
 					cout << "Hit,";
@@ -109,12 +125,13 @@ int main(void)
 				}
 			}
 			cout << "\n";
+			//３つ当たったらループ終了
 			if (hit == 3)
 			{
 				cout << "3Hit!!\n";
 				break;
 			}
-
+			//ターン切り替え
 			cout << "\n";
 			trunFlag = true;
 		}
@@ -122,6 +139,7 @@ int main(void)
 
 	}
 
+	//勝ち負けを表示
 	if (judgementFlag)
 	{
 		cout << "PLAYER WINNER\n";
